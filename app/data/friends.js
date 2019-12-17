5b908fe2dfe4a188c39c4115e6c72feed114ea2a
mike881205 <mike881205@gmail.com>
2019-12-16 21:48:34 -0500

    updated server.js routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,13 @@ let friendData = [
     {
         name: "The Dude",
         photo: "",
-        scores: []
+        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
+    {
+        name: "Not the Dude",
+        photo: "",
+        scores: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+    }
 ]
 
 module.exports = friendData;
