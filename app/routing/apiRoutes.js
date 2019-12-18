1bf7877f2c1945ceb671b33ee44a9518f89e783f
mike881205 <mike881205@gmail.com>
2019-12-17 22:48:19 -0500

    added code in apiRoutes to compare the users scores with the scores in the friends scores array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,14 +11,14 @@ let friendsData = require("../data/friends.js");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
 
@@ -30,10 +30,49 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
+    
+    let scoreArray = []
+    
+    for (let i = 0; i < friendsData.length; i++) {
+
+      let sum = 0;
+      let differenceArr = []
+      let score = 0
+      let diffTotal;
+      let difference;
+
+      for (let j = 0; j < friendsData[i].scores.length; j++) {
+
+        let friendScore = parseInt(friendsData[i].scores[j])
+        let newScore = parseInt(req.body.scores[j])
+        difference = (newScore - friendScore)
+
+        if (difference < 0) {
+          difference = difference * -1;
+        }
+
+      }
+
+      differenceArr.push(difference)
+
+      for (let i = 0; i < differenceArr.length; i++) {
+
+        diffTotal = sum += differenceArr[i]
+
+        score = score + diffTotal
+
+      }
+
+      scoreArray.push(score)
+
+    }
+
+    console.log(scoreArray)
+
     friendsData.push(req.body);
   });
 };
