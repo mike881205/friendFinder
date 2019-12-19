07a4bc52d27f09ced02ccacb2502f16950e6efb0
mike881205 <mike881205@gmail.com>
2019-12-19 16:28:09 -0500

    updated friends array in friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,54 @@
 let friendData = [
     {
-        name: "The Dude",
-        photo: "https://secure.i.telegraph.co.uk/multimedia/archive/03439/dude6_3439641b.jpg",
-        scores: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+        name: "Adventure",
+        photo: "https://avatarfiles.alphacoders.com/169/169013.jpg",
+        scores: ["5", "3", "1", "4", "2", "5", "3", "1", "4", "2"]
     },
     {
-        name: "Not the Dude",
-        photo: "https://m.media-amazon.com/images/M/MV5BYTk4OTE5ZDQtMzRkNC00NTM5LTkxYmUtNWYzYjc4OTQyODZlXkEyXkFqcGdeQXVyNDc3NDQzMzU@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
-        scores: ["5", "4", "3", "2", "1", "5", "4", "3", "2", "1"]
+        name: "FPS",
+        photo: "https://assets.change.org/photos/2/jv/pv/vTjvPVYmmKOwLwe-128x128-noPad.jpg?1552404933",
+        scores: ["4", "5", "2", "3", "1", "4", "5", "2", "3", "1"]
     },
     {
-        name: "Miguel",
-        photo: "https://vignette.wikia.nocookie.net/disney/images/0/00/Profile_-_Miguel_Rivera.jpg/revision/latest?cb=20190315012208",
-        scores: ["1", "2", "3", "4", "5", "4", "3", "2", "1", "2"]
+        name: "Fighting",
+        photo: "https://at-cdn-s01.audiotool.com/2014/09/07/documents/SwaOa0gF51Ai06D6LDLyJHubdbWXNu/0/cover256x256-9cbba6b2878944c6b29e14bdead8ac32.jpg",
+        scores: ["2", "3", "5", "4", "1", "2", "3", "5", "4", "1"]
     },
     {
-        name: "dirk diggler",
-        photo: "https://grantland.com/wp-content/uploads/2014/12/boogie-nights-starring-mark-wahlberg-11.jpg?w=694",
-        scores: ["5", "4", "3", "2", "1", "2", "3", "4", "5", "4"]
+        name: "RPG",
+        photo: "https://static.myfigurecollection.net/pics/figure/big/589112.jpg",
+        scores: ["4", "1", "2", "5", "3", "4", "1", "2", "5", "3"]
     },
+    {
+        name: "Puzzle",
+        photo: "https://pbs.twimg.com/profile_images/489559855439831041/LiB3cmTE.jpeg",
+        scores: ["3", "2", "1", "4", "5", "3", "2", "1", "4", "5"]
+    },
+    {
+        name: "Adventure 2",
+        photo: "https://avatarfiles.alphacoders.com/548/54820.jpg",
+        scores: ["4", "2", "1", "3", "1", "4", "2", "1", "3", "1"]
+    },
+    {
+        name: "FPS 2",
+        photo: "https://pbs.twimg.com/profile_images/3081282612/80abcd866c4f95084a7eb62f0c529991.jpeg",
+        scores: ["3", "4", "1", "2", "1", "3", "4", "1", "2", "1"]
+    },
+    {
+        name: "Fighting 2",
+        photo: "https://at-cdn-s01.audiotool.com/2014/09/07/documents/SwaOa0gF51Ai06D6LDLyJHubdbWXNu/0/cover256x256-9cbba6b2878944c6b29e14bdead8ac32.jpg",
+        scores: ["1", "2", "4", "3", "1", "1", "2", "4", "3", "1"]
+    },
+    {
+        name: "RPG 2",
+        photo: "https://static.myfigurecollection.net/pics/figure/big/234221.jpg",
+        scores: ["3", "1", "1", "4", "2", "3", "1", "1", "4", "2"]
+    },
+    {
+        name: "Puzzle 2",
+        photo: "https://pbs.twimg.com/profile_images/513059222053867520/nl6eHH2w.png",
+        scores: ["2", "1", "1", "3", "4", "2", "1", "1", "3", "4"]
+    }
     // {
     //     name: "NAME",
     //     photo: "",
